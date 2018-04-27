13a109a710e4c32e3cbe4a552c9213e9321404be
Adrian Messado <adrian.messado@gmail.com>
2018-04-27 13:52:57 -0700

    fill out class.js, define variables, add async functions

--- src/class.js
+++ src/class.js
@@ -3,8 +3,20 @@ class BetterDoctor {
     this.prop = parameter;
   }
 
-  method() {
-    this.prop = value;
+  search(dName, condition) {
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
   }
 }
 
