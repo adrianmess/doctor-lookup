2670f3bcee2625206f4fc6b6c437b81ca706b62c
Adrian Messado <adrian.messado@gmail.com>
2018-04-27 14:23:49 -0700

    use console log to confirm JSON response output is received

--- src/class.js
+++ src/class.js
@@ -1,7 +1,7 @@
 class BetterDoctor {
   constructor() {}
 
-  search(dName, condition) {
+  search(Dname, Mcondition) {
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
       let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`
--- src/main.js
+++ src/main.js
@@ -6,8 +6,21 @@ import { BetterDoctor } from './class.js';
 
 $(document).ready(function() {
   $("#buttonSub").click(function(){
+    event.preventDefault();
+    let doctorApi = new BetterDoctor();
+    let doctorSearch = doctorApi.search(Dname, Mcondition);
+    // Note: doctorSearch = Promise from class.js
     let Dname = $("#Dname").val();
+    let Mcondition = $('#Mcondition').val();
     $('#Dname').val('');
+    $('#Mcondition').val('');
     console.log($('#Dname').val());
+
+    doctorSearch.then(function(response){
+      let body = JSON.parse(response);
+      console.log(body)
+
+    })
+
   });
 });
