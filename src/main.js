d1fdb23e02b5b072907fe7dcdf9153bf5ef49ca2
Adrian Messado <adrian.messado@gmail.com>
2018-04-29 16:19:21 -0700

    get for loop in jquery response to return first name of Dr

--- src/main.js
+++ src/main.js
@@ -11,15 +11,26 @@ $(document).ready(function() {
     $('#Mcondition').val('');
     console.log(Dname);
     console.log(Mcondition);
-    var url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=22065b1d7a0d09da2af7c21410b2ddad`;
-    console.log(url);
+
 
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=22065b1d7a0d09da2af7c21410b2ddad`).then(function(response) {
-      // $('.showResults').text(`${response.body}`);
+      // $('.showResults').text(`${response.data}`);
       // console.log(`${response}`)
-      for (var index in response) {
-        console.log(response[index]);
+
+      for (var index in response.data) {
+        let test = response.data[index].profile.first_name;
+        $('.showResults').text(`${response.data[index].profile.first_name}`);
+        // console.log(response[index]);
+        console.log(test);
       }
+
+      // for (var index in response) {
+      //   for ( var practices in index) {
+      //     let test = practices[0];
+      //     // $('.showResults').text(`${response.data.profile.first_name}`);
+      //     console.log(test);
+      //   }
+      // }
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
