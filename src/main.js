4c90d555d648edd9e6091f7986e9dd9f5d423d26
Adrian Messado <adrian.messado@gmail.com>
2018-04-29 23:03:09 -0700

    add formatting to index.html, cleanup code

--- src/main.js
+++ src/main.js
@@ -5,16 +5,13 @@ import $ from 'jquery';
 
 $(document).ready(function() {
   $('#buttonSub').click(function() {
+    $('.clear').remove();
     let Dname = $("#Dname").val();
     let Mcondition = $('#Mcondition').val();
     $('#Dname').val('');
     $('#Mcondition').val('');
-    console.log(Dname);
-    console.log(Mcondition);
-
 
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=22065b1d7a0d09da2af7c21410b2ddad`).then(function(response) {
-
       $.each(response.data, function(i, data){
         var firstName = data.profile.first_name;
         var lastName = data.profile.last_name;
@@ -25,6 +22,7 @@ $(document).ready(function() {
         let street = data.practices[0].visit_address.street;
         let zip = data.practices[0].visit_address.zip;
         let address = (street + '<br>' + city + ', ' + state + ', ' + zip);
+        let newP = data.practices[0].accepts_new_patients;
         let siteUnd = data.practices[0].website;
         let website = function (siteUnd) {
           let missing = siteUnd;
@@ -35,11 +33,11 @@ $(document).ready(function() {
           }
         };
         let finalSite = website(siteUnd);
-        $('.showResults').append(firstName + ' ' + lastName + contact + phone + '<br>' + address + '<br>' + 'Website: ' + finalSite + '<p>');
+        $('.showResults').append('<span class=\'clear\'>' + '<span class=\'names\'>' + firstName + ' ' + lastName + '</span>' + contact + phone + '<br>' + address + '<br>' + 'Accepting new patients: ' + newP + '<br>' + 'Website: ' + finalSite + '<p></span>');
       });
 
     }).fail(function(error) {
-      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
+      $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`)
     });
   });
 });
