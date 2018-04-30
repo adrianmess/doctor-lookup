65ab4e9dbb3fa3682b3d6bc8a63e2e92ec9066d8
Adrian Messado <adrian.messado@gmail.com>
2018-04-29 21:57:19 -0700

    remove unneeded comments

--- src/main.js
+++ src/main.js
@@ -14,24 +14,8 @@ $(document).ready(function() {
 
 
     $.get(`https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=22065b1d7a0d09da2af7c21410b2ddad`).then(function(response) {
-      // $('.showResults').text(`${response.data}`);
-      // console.log(`${response}`)
-
-      // for (var index in response.data) {
-      //   // let test = response.data[index].profile.first_name + ' ' + response.data[index].profile.last_name + '<br>' + response.data[index].practices.name + '<br>' ;
-      //    let practice = response.data[index].practices.name;
-      //
-      //
-      //   // $('.showResults').append(test + '<br>');
-      //
-      //   console.log(practice);
-      // }
-
-      // var allData = response.data;
-      // console.log(allData);
 
       $.each(response.data, function(i, data){
-        // $.each(practices, function(i2, ))
         var firstName = data.profile.first_name;
         var lastName = data.profile.last_name;
         var contact = ('<br>' + 'Contact Info: ' + '<br>' + 'Phone: ');
@@ -41,21 +25,19 @@ $(document).ready(function() {
         let street = data.practices[0].visit_address.street;
         let zip = data.practices[0].visit_address.zip;
         let address = (street + '<br>' + city + ', ' + state + ', ' + zip);
-        let website = ('Website: ' + data.practices[0].website;);
-        // var address = data.practices[1].visit_address
-        $('.showResults').append(firstName + ' ' + lastName + contact + phone + '<br>' + address + '<br>' + website + '<p>');
-        console.log(data.practices[0].website);
+        let siteUnd = data.practices[0].website;
+        let website = function (siteUnd) {
+          let missing = siteUnd;
+          if (typeof missing === "undefined") {
+            return "No website listed"
+          } else {
+            return missing;
+          }
+        };
+        let finalSite = website(siteUnd);
+        $('.showResults').append(firstName + ' ' + lastName + contact + phone + '<br>' + address + '<br>' + 'Website: ' + finalSite + '<p>');
       });
 
-
-
-      // for (var index in response) {
-      //   for ( var practices in index) {
-      //     let test = practices[0];
-      //     // $('.showResults').text(`${response.data.profile.first_name}`);
-      //     console.log(test);
-      //   }
-      // }
     }).fail(function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error.responseText}. Please try again.`);
     });
