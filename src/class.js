589c2cfb8c783b10d95b7c89084d5480e2d1844d
Adrian Messado <adrian.messado@gmail.com>
2018-05-02 09:20:44 -0700

    change class and id names to match variable and object literals in class.js - main.js

--- src/class.js
+++ src/class.js
@@ -1,14 +1,10 @@
 class BetterDoctor{
   constructor(){}
 
-  doctorCall(){
+  doctorCall(doctorName, medicalCondition){
     return new Promise(function(resolve, reject){
       let request = new XMLHttpRequest();
-      let url =`https://api.betterdoctor.com/2016-03-01/doctors/df9c5372f154ea5c0c0467308c72374f?user_key=22065b1d7a0d09da2af7c21410b2ddad`
-
-       // `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalCondition}&location=45.539,-122.604,25&user_location=45.539,-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-
-      request.responseType='json';
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalCondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
       request.onload = function(){
         if(this.status === 200){
           console.log(request.response)
