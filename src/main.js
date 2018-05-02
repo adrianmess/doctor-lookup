c6bd8578528e7c3bdbbab4cb88bb881104f974c7
Adrian Messado <adrian.messado@gmail.com>
2018-05-01 21:08:36 -0700

    comment out doctorCall function, function was moved to class.js

--- src/main.js
+++ src/main.js
@@ -12,22 +12,22 @@ $(document).ready(function() {
     $('#doctorName').val('');
     $('#medicalCondition').val('');
 
-    function doctorCall() {
-      return new Promise(function(resolve, reject) {
-        let request = new XMLHttpRequest();
-        let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalCondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-
-        request.onload = function() {
-          if (this.status === 200) {
-            resolve(request.response);
-          } else {
-            reject(Error(request.statusText));
-          }
-        }
-        request.open("GET", url, true);
-        request.send();
-      });
-    }
+    // function doctorCall() {
+    //   return new Promise(function(resolve, reject) {
+    //     let request = new XMLHttpRequest();
+    //     let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalCondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+    //
+    //     request.onload = function() {
+    //       if (this.status === 200) {
+    //         resolve(request.response);
+    //       } else {
+    //         reject(Error(request.statusText));
+    //       }
+    //     }
+    //     request.open("GET", url, true);
+    //     request.send();
+    //   });
+    // }
 
     doctorCall ()
       .then(function(response) {
