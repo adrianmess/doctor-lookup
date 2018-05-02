7133fc93053a3f0417b424bf6dbc0638bdb634f3
Adrian Messado <adrian.messado@gmail.com>
2018-05-01 20:57:31 -0700

    change var's to let, change variable names and function name to be more descriptive

---
+++ src/class.js
@@ -0,0 +1,20 @@
+class BetterDoctor{
+  constructor(){}
+
+  doctorCall(Dname, Mcondition){
+    return new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${Dname}&query=${Mcondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
+      request.onload = function(){
+        if(this.status === 200){
+          resolve(request.response);
+        } else {
+          reject(Error(request.statusText));
+        }
+      }
+      request.open("GET", url, true);
+      request.send();
+    });
+  }
+}
+export {BetterDoctor}
