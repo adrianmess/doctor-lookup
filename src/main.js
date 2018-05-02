f1586fb0d03fa6bb0737767fbe02219b201a7b58
Adrian Messado <adrian.messado@gmail.com>
2018-05-01 23:18:12 -0700

    resolve  Cannot call a class as a function error

--- src/main.js
+++ src/main.js
@@ -12,26 +12,8 @@ $(document).ready(function() {
     $('#doctorName').val('');
     $('#medicalCondition').val('');
 
-    let bDoctorApi = BetterDoctor();
+    let bDoctorApi = new BetterDoctor();
     let bDoctorData = bDoctorApi.doctorCall(doctorName, medicalCondition);
-
-    // function doctorCall() {
-    //   return new Promise(function(resolve, reject) {
-    //     let request = new XMLHttpRequest();
-    //     let url = `https://api.betterdoctor.com/2016-03-01/doctors?name=${doctorName}&query=${medicalCondition}&location=45.539%2C-122.604%2C25&user_location=45.539%2C-122.604&sort=best-match-asc&skip=0&limit=10&user_key=${process.env.exports.apiKey}`;
-    //
-    //     request.onload = function() {
-    //       if (this.status === 200) {
-    //         resolve(request.response);
-    //       } else {
-    //         reject(Error(request.statusText));
-    //       }
-    //     }
-    //     request.open("GET", url, true);
-    //     request.send();
-    //   });
-    // }
-
 
       bDoctorData.then(function(response) {
         let body = JSON.parse(response);
